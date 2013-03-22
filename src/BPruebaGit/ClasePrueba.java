ba961660fa910caf6f4bfd3729b72c3657a98f0a
Iliondos <iliondos@hotmail.com>
2013-03-22 21:09:15 +0100

    Un commit que esperemos vaya bien.

---
+++ src/BPruebaGit/ClasePrueba.java
@@ -0,0 +1,14 @@
+package BPruebaGit;
+
+public class ClasePrueba {
+
+	/**
+	 * @param args
+	 * Clase que estamos haciendo para probar git
+	 */
+	public static void main(String[] args) {
+		System.out.println("Probando voy, probando vengo. ");
+
+	}
+
+}
